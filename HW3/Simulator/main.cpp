48d750ac6e649b50239b39931224a0e8c88b7bde
Charles Wang <42820206+wcz1996@users.noreply.github.com>
2019-03-28 15:22:51 -0400

    Add files via upload

---
+++ HW3/Simulator/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Simulator
+//
+//  Created by CHENZHANG WANG on 11/19/18.
+//  Copyright © 2018 CHENZHANG WANG. All rights reserved.
+//
+
+#include <iostream>
+
+class cube
+{
+public:
+    double 
+}
+using namespace std;
+int main(int argv, char *argv)
+{
+    
+}
